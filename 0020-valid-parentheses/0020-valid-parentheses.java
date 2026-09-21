ec8b1319b212f467195e8d7d326c18aa06c7655c
ADHD.Dev <sumanmandal8955@gmail.com>
2026-09-21 17:57:08 +0530

    Time: 5 ms (13.25%), Space: 43 MB (92.91%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+
+        Map<Character, Character> pairs = Map.of(
+            '(', ')',
+            '[', ']',
+            '{', '}'
+        );
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(char c : s.toCharArray()){
+            if(pairs.containsKey(c)){
+                stack.push(pairs.get(c));
+            } else {
+                if(stack.isEmpty() || stack.pop() != c){
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
